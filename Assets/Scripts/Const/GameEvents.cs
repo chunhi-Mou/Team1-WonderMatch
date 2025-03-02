d6c1f1fb84a17495854dd8f6c340d6337ba9b8d7
Catkina <anhthu67144@gmail.com>
2025-03-02 20:30:59 +0700

    Update Script: GameEvents

--- Assets/Scripts/Const/GameEvents.cs
+++ Assets/Scripts/Const/GameEvents.cs
@@ -2,13 +2,21 @@ using System;
 using UnityEngine;
 
 public static class GameEvents{
-    public static event Action<CardData> OnTileSelected;
+    public static event Action<Tile> OnTileSelected;
     public static event Action<Transform> OnFoundPosOfTile;
+    public static event Action OnTileDoneMoving;
+    public static event Action OnLoseGame;
 
-    public static void OnTileSelectedInvoke(CardData cardData) {
-        OnTileSelected.Invoke(cardData);
+    public static void OnTileSelectedInvoke(Tile tile) {
+        OnTileSelected.Invoke(tile);
     }
     public static void OnFoundPosOfTileInvoke(Transform target) {
         OnFoundPosOfTile.Invoke(target);
+    }
+    public static void OnTileDoneMovingInvoke(){
+        OnTileDoneMoving.Invoke();
+    }
+    public static void OnLoseGameInvoke(){
+        OnLoseGame.Invoke();
     }
 }
