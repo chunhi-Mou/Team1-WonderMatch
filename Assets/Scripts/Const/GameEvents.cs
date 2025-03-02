64249d27bedce5cd6c5838cd0c50411e5543828c
Chu Tuyết Nhi <chunhi.dev@gmail.com>
2025-03-02 20:27:40 +0700

    Fix Bug: Tile
    - tất cả đều bị trigger event

--- Assets/Scripts/Const/GameEvents.cs
+++ Assets/Scripts/Const/GameEvents.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 public static class GameEvents{
     public static event Action<Tile> OnTileSelected;
     public static event Action<Transform> OnFoundPosOfTile;
+    public static event Action OnTileDoneMoving;
 
     public static void OnTileSelectedInvoke(Tile tile) {
         OnTileSelected.Invoke(tile);
     }
     public static void OnFoundPosOfTileInvoke(Transform target) {
         OnFoundPosOfTile.Invoke(target);
+    }
+    public static void OnTileDoneMovingInvoke() {
+        OnTileDoneMoving.Invoke();
     }
 }
