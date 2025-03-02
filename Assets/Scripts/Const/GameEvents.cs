e8cd63f98d389b7d1d375249502642187c305142
Chu Tuyết Nhi <chunhi.dev@gmail.com>
2025-03-02 16:05:15 +0700

    Update GameEvents.cs
    -use Tile instead of cardData

--- Assets/Scripts/Const/GameEvents.cs
+++ Assets/Scripts/Const/GameEvents.cs
@@ -2,11 +2,11 @@ using System;
 using UnityEngine;
 
 public static class GameEvents{
-    public static event Action<CardData> OnTileSelected;
+    public static event Action<Tile> OnTileSelected;
     public static event Action<Transform> OnFoundPosOfTile;
 
-    public static void OnTileSelectedInvoke(CardData cardData) {
-        OnTileSelected.Invoke(cardData);
+    public static void OnTileSelectedInvoke(Tile tile) {
+        OnTileSelected.Invoke(tile);
     }
     public static void OnFoundPosOfTileInvoke(Transform target) {
         OnFoundPosOfTile.Invoke(target);
