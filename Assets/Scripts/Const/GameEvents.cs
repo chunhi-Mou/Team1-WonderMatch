1999402dfcf55234c52827a255d3f581c21a1092
Chu Tuyết Nhi <chunhi.dev@gmail.com>
2025-03-02 14:17:58 +0700

    Update GameEvents: Add gameObj khi OnTileSelectedInvoke

--- Assets/Scripts/Const/GameEvents.cs
+++ Assets/Scripts/Const/GameEvents.cs
@@ -2,13 +2,12 @@ using System;
 using UnityEngine;
 
 public static class GameEvents{
-    public static event Action OnTileSelected;
+    public static event Action<GameObject> OnTileSelected;
     public static event Action<Transform> OnFoundPosOfTile;
 
-    public static void OnTileSelectedInvoke() {
-        OnTileSelected.Invoke();
+    public static void OnTileSelectedInvoke(GameObject selectedTile) {
+        OnTileSelected.Invoke(selectedTile);
     }
-
     public static void OnFoundPosOfTileInvoke(Transform target) {
         OnFoundPosOfTile.Invoke(target);
     }
