4ca53236fb3ffb539c2fb121f98d5ae7b49bb4c2
Chu Tuyết Nhi <chunhi.dev@gmail.com>
2025-03-03 23:56:42 +0700

    Small Update cs

--- Assets/Scripts/Const/GameEvents.cs
+++ Assets/Scripts/Const/GameEvents.cs
@@ -2,13 +2,13 @@ using System;
 using UnityEngine;
 
 public static class GameEvents {
-    public static event Action<Card> OnTileSelected;
+    public static event Action<Card> OnCardSelected;
     public static event Action<Transform> OnFoundPosOfCard;
     public static event Action OnCardDoneMoving;
     public static event Action OnLoseGame;
 
-    public static void OnCardSelectedInvoke(Card tile) {
-        OnTileSelected?.Invoke(tile);
+    public static void OnCardSelectedInvoke(Card card) {
+        OnCardSelected?.Invoke(card);
     }
     public static void OnFoundPosOfCardInvoke(Transform target) {
         OnFoundPosOfCard?.Invoke(target);
