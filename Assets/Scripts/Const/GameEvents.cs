04a6664a5ddfd67bb837fc742d50eb8958fe8bff
Chu Tuyết Nhi <chunhi.dev@gmail.com>
2025-03-06 22:38:58 +0700

    Update cs: Add modify Speed

--- Assets/Scripts/Const/GameEvents.cs
+++ Assets/Scripts/Const/GameEvents.cs
@@ -3,15 +3,15 @@ using UnityEngine;
 
 public static class GameEvents {
     public static event Action<Card> OnCardSelected;
-    public static event Action<Transform> OnFoundPosOfCard;
+    public static event Action<Transform, float> OnFoundPosOfCard;
     public static event Action OnCardDoneMoving;
     public static event Action OnLoseGame;
 
     public static void OnCardSelectedInvoke(Card card) {
         OnCardSelected?.Invoke(card);
     }
-    public static void OnFoundPosOfCardInvoke(Transform target) {
-        OnFoundPosOfCard?.Invoke(target);
+    public static void OnFoundPosOfCardInvoke(Transform target, float _duration) {
+        OnFoundPosOfCard?.Invoke(target, _duration);
     }
     public static void OnCardDoneMovingInvoke() {
         OnCardDoneMoving?.Invoke();
