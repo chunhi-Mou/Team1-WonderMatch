91b7febc97192957298e62a8148bf1791dec57ff
Chu Tuyết Nhi <chunhi.dev@gmail.com>
2025-03-09 19:04:21 +0700

    Update GameEvents

--- Assets/Scripts/Const/GameEvents.cs
+++ Assets/Scripts/Const/GameEvents.cs
@@ -4,14 +4,14 @@ using DG.Tweening;
 
 public static class GameEvents {
     public static event Action<Card> OnCardSelected;
-    public static event Action<Transform, float, Ease> OnFoundPosOfCard;
+    public static event Action<Vector3, float, Ease> OnFoundPosOfCard;
     public static event Action OnCardDoneMoving;
     public static event Action OnLoseGame;
 
     public static void OnCardSelectedInvoke(Card card) {
         OnCardSelected?.Invoke(card);
     }
-    public static void OnFoundPosOfCardInvoke(Transform target, float _duration, Ease _ease) {
+    public static void OnFoundPosOfCardInvoke(Vector3 target, float _duration, Ease _ease) {
         OnFoundPosOfCard?.Invoke(target, _duration, _ease);
     }
     public static void OnCardDoneMovingInvoke() {
