09186385196b09cc04cdb4de5dbe4ba10da62364
Chu Tuyết Nhi <chunhi.dev@gmail.com>
2025-03-10 23:30:29 +0700

    Update cs: Managers: GameStates: Single

---
+++ Assets/Scripts/Core/SingleModeManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class SingleModeManager : GameStateBase {
+    public static SingleModeManager Instance { get; private set; }
+
+    private void Awake() {
+        if (Instance == null) {
+            Instance = this;
+        } else {
+            Destroy(gameObject);
+            return;
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+    public void LoseGame() {
+        this.TogglePause();
+        //Gọi hàm trừ tim
+        Debug.Log("Lose!");
+    }
+    public void WinGame() {
+        this.TogglePause();
+        //Gọi Hàm cộng coins
+        Debug.Log("Win");
+    }
+}
