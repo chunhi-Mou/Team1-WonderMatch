a734536f64f22c508dd6eb190cacf500068fbf05
Chu Tuyết Nhi <chunhi.dev@gmail.com>
2025-03-13 17:11:51 +0700

    Add Coins UI

---
+++ Assets/Scripts/UI/CoinsUI.cs
@@ -0,0 +1,50 @@
+using TMPro;
+using UnityEngine;
+
+public class CoinsUI : MonoBehaviour {
+    [SerializeField] RectTransform spendCoinsUI;
+    [SerializeField] RectTransform xButton;
+    [SerializeField] RectTransform adButton;
+    [SerializeField] RectTransform spendButton;
+    [SerializeField] RectTransform[] powerImages;
+    [SerializeField] TextMeshProUGUI coinTxt;
+    private void OnEnable() {
+        GameEvents.OnSpendCoinsNeeded += TurnOnSpendCoinsUI;
+    }
+    private void OnDisable() {
+        GameEvents.OnSpendCoinsNeeded -= TurnOnSpendCoinsUI;
+    }
+    private PowerType currPowerType;
+    public void TurnOnSpendCoinsUI(PowerType powerType) {
+        coinTxt.text = CoinsManager.Instance.currCoins.ToString();
+        spendCoinsUI.gameObject.SetActive(true);
+        this.currPowerType = powerType;
+
+        foreach (var img in powerImages) {
+            img.gameObject.SetActive(false);
+        }
+        powerImages[(int)powerType].gameObject.SetActive(true);
+
+        if(CoinsManager.Instance.CanBuyPower()) {
+            spendButton.gameObject.SetActive(true);
+            adButton.gameObject.SetActive(false);
+        } else {
+            spendButton.gameObject.SetActive(false);
+            adButton.gameObject.SetActive(true);
+        }
+    }
+    public void OnSpendButtonPress() {
+        if (CoinsManager.Instance.CanBuyPower()) {
+            PowerUpsManager.Instance.ResetPower(currPowerType);
+            spendCoinsUI.gameObject.SetActive(false);
+        }
+    }
+    public void OnXPress() {
+        spendCoinsUI.gameObject.SetActive(false);
+    }
+    public void OnAdButtonPress() {
+        PowerUpsManager.Instance.ResetPower(currPowerType);
+        Debug.Log("YOU WATCHED AD");
+        spendCoinsUI.gameObject.SetActive(false);
+    }
+}
