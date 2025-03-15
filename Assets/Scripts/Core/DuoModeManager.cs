23563192e5e38cdc554658e15316ac30ee0c6ef3
Chu Tuyết Nhi <chunhi.dev@gmail.com>
2025-03-15 09:25:00 +0700

    Update scripts: Game Mode
    - Tối ưu dùng Interface

---
+++ Assets/Scripts/Core/DuoModeManager.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DuoModeManager : MonoBehaviour, IGameMode {
+    #region Singleton - Dont destroy
+    public static DuoModeManager instance { get; private set; }
+
+    private void Awake() {
+        if (instance == null) {
+            instance = this;
+        } else {
+            Destroy(gameObject);
+            return;
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+    #endregion
+    private bool isPaused = false;
+    public bool IsPaused => isPaused;
+
+    public void TogglePause() {
+        isPaused = !isPaused;
+        Time.timeScale = isPaused ? 0 : 1;
+    }
+
+    public void ResetGame() {
+        Time.timeScale = 1;
+        isPaused = false;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
---
+++ Assets/Scripts/Core/GameModeManager.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public interface IGameModeObject {
+    void RegisterToGameMode();
+}
+
+public class GameModeManager : MonoBehaviour {
+    #region Singleton - Dont destroy
+    public static GameModeManager instance { get; private set; }
+
+    private void Awake() {
+        if (instance == null) {
+            instance = this;
+        } else {
+            Destroy(gameObject);
+            return;
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+    #endregion
+    public IGameMode gameMode;
+    [SerializeField] GameObject SingleMode;
+    [SerializeField] GameObject DuoMode;
+    public void OnSingleModeSelected() {
+        SceneManager.LoadScene(1);
+        SingleMode.SetActive(true);
+        DuoMode.SetActive(false);
+        gameMode = SingleModeManager.instance;
+        SingleModeManager.instance.TurnOnObjsOfSingleMode();
+    }
+    public void OnDuoModeSelected() {
+        SceneManager.LoadScene(1);
+        SingleMode.SetActive(false);
+        DuoMode.SetActive(true);
+        gameMode = DuoModeManager.instance;
+    }
+}
