7138380a96935283fc38fdb9599249896455e2c7
Chu Tuyết Nhi <chunhi.dev@gmail.com>
2025-03-15 09:25:44 +0700

    Update scripts: Power-ups
    - interface

---
+++ Assets/Scripts/Gameplay/MagicPowerUp.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class MagicPowerUp : IPowerUp {
+    private int count = 3;
+    private Stack stack;
+
+    public MagicPowerUp() {
+        stack = Object.FindObjectOfType<Stack>();
+        count = PlayerPrefs.GetInt(SavedData.MagicPowerCount, 3);
+    }
+
+    public void Use() {
+        if (count > 0) {
+            stack.StackMagicHandler();//Đồng thời Invoke cho Board
+            count--;
+            SaveData();
+        } else {
+            GameEvents.OnSpendCoinsNeededInvoke(PowerType.Magic);
+        }
+    }
+
+    public void ResetCount(int maxCount) {
+        count = maxCount;
+    }
+
+    public int GetCount() {
+        return count;
+    }
+    public void SaveData() {
+        PlayerPrefs.SetInt(SavedData.MagicPowerCount, count);
+        PlayerPrefs.Save();
+    }
+}
