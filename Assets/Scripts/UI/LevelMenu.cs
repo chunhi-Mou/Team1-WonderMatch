422b341209d6a8342a28b02d950e6b9c10c7184d
Chu Tuyết Nhi <chunhi.dev@gmail.com>
2025-03-18 14:19:45 +0700

    update

--- Assets/Scripts/UI/LevelMenu.cs
+++ Assets/Scripts/UI/LevelMenu.cs
@@ -1,30 +1,29 @@
-using DG.Tweening;
 using UnityEngine;
+using DG.Tweening;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
-public class LevelMenu : MonoBehaviour
-{
+
+public class LevelMenu : MonoBehaviour {
     public Button[] buttons;
-    private void Awake()
-    {
-        int unlockedLevel = PlayerPrefs.GetInt(SavedData.UnlockedLevel, 1); 
-        for (int i=0; i<buttons.Length; i++){
-            buttons[i].interactable = false;
-        }
-        for (int i=0; i<unlockedLevel; i++){
-            buttons[i].interactable = true;
+
+    private void Awake() {
+        int unlockedLevel = LevelManager.UnlockedLevels;
+        for (int i = 0; i < buttons.Length; i++) {
+            buttons[i].interactable = i < unlockedLevel;
         }
     }
-    public void OpenLevel(int levelID)
-    {
+
+    public void OpenLevel(int levelID) {
+        if (levelID > LevelManager.UnlockedLevels) return;
+
         SceneManager.sceneLoaded += OnSceneReloaded;
-        SceneManager.LoadScene(2);
-        if (GameModeManager.instance.gameMode == (IGameMode)SingleModeManager.instance)
-            SingleModeManager.instance.TurnOnObjsOfSingleMode();
+        SceneManager.LoadScene("InGame");
     }
+
     private void OnSceneReloaded(Scene scene, LoadSceneMode mode) {
-        if (GameModeManager.instance.gameMode == (IGameMode)SingleModeManager.instance)
+        if (GameModeManager.instance.gameMode == (IGameMode)SingleModeManager.instance) {
             SingleModeManager.instance.TurnOnObjsOfSingleMode();
+        }
         SceneManager.sceneLoaded -= OnSceneReloaded;
     }
 }
