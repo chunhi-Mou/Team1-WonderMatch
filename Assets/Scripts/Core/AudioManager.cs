a6d91bee97d0bf1ccd4bf3e29aa6f51499392bda
Chu Tuyết Nhi <chunhi.dev@gmail.com>
2025-03-23 18:18:19 +0700

    update cs: Fix bug: Audio

--- Assets/Scripts/Core/AudioManager.cs
+++ Assets/Scripts/Core/AudioManager.cs
@@ -4,7 +4,6 @@ using System;
 using System.Collections.Generic;
 using UnityEngine.UI;
 using UnityEngine.Events;
-using UnityEngine.SceneManagement;
 
 public class AudioManager : MonoBehaviour {
     [System.Serializable]
@@ -13,14 +12,11 @@ public class AudioManager : MonoBehaviour {
         public AudioMixerGroup outputMixer;
         public AudioClip clip;
         public bool loop = false;
-        [Range(0f, 1f)] public float volume = 1f;
-        [Range(0.1f, 3f)] public float pitch = 1f;
         [HideInInspector] public AudioSource source;
-        
     }
+
     public Sound[] sounds;
     public AudioMixer audioMixer;
-
     private Dictionary<SoundEffect, AudioSource> soundDict;
 
     #region Singleton
@@ -34,18 +30,18 @@ public class AudioManager : MonoBehaviour {
             return;
         }
         InitSounds();
-        LoadSettings();
     }
     #endregion
-    
+
+    private void Start() {
+        LoadSettings();
+    }
     private void InitSounds() {
         soundDict = new Dictionary<SoundEffect, AudioSource>();
 
         foreach (var sound in sounds) {
             AudioSource source = gameObject.AddComponent<AudioSource>();
             source.clip = sound.clip;
-            source.volume = sound.volume;
-            source.pitch = sound.pitch;
             source.loop = sound.loop;
             source.outputAudioMixerGroup = sound.outputMixer;
 
@@ -84,76 +80,24 @@ public class AudioManager : MonoBehaviour {
             }
         }
     }
-    public void SetMasterVolume(float volume) {
-        float dB = Mathf.Lerp(-80f, 0f, volume);
-        audioMixer.SetFloat("MasterVolume", dB);
-        PlayerPrefs.SetFloat("MasterVolume", volume);
-        PlayerPrefs.Save();
-    }
-
-    public void SetMusicVolume(float volume) {
-        float dB = Mathf.Lerp(-80f, 0f, volume);
-        audioMixer.SetFloat("MusicVolume", dB);
-        PlayerPrefs.SetFloat("MusicVolume", volume);
-        PlayerPrefs.Save();
-    }
-
-    public void SetSFXVolume(float volume) {
-        float dB = Mathf.Lerp(-80f, 0f, volume);
-        audioMixer.SetFloat("SFXVolume", dB);
-        PlayerPrefs.SetFloat("SFXVolume", volume);
-        PlayerPrefs.Save();
-    }
 
     private void LoadSettings() {
-        float master = PlayerPrefs.GetFloat("MasterVolume", 1f);
-        float music = PlayerPrefs.GetFloat("MusicVolume", 1f);
-        float sfx = PlayerPrefs.GetFloat("SFXVolume", 1f);
-
-        SetMasterVolume(master);
-        SetMusicVolume(music);
-        SetSFXVolume(sfx);
+        ToggleMusic(PlayerPrefs.GetInt(SavedData.MusicToggle, 1) == 1);
+        ToggleSFX(PlayerPrefs.GetInt(SavedData.SFXToggle, 1) == 1);
     }
-    public void SetUpSlider(Slider slider, string prefsKey) {
-        float savedValue = PlayerPrefs.GetFloat(prefsKey, 1f);
-        slider.SetValueWithoutNotify(savedValue);
-    }
-
     public void SetUpToggle(Toggle toggle, string prefsKey, UnityAction<bool> toggleAction) {
         bool isOn = PlayerPrefs.GetInt(prefsKey, 1) == 1;
         toggle.isOn = isOn;
         toggle.onValueChanged.AddListener(toggleAction);
     }
     public void ToggleMusic(bool isOn) {
-        float defaultVolume = 1f; 
-        float volume = isOn ? defaultVolume : 0f; 
-        SetMusicVolume(volume);
-        PlayerPrefs.SetFloat(SavedData.MusicVolume, volume); 
+        audioMixer.SetFloat("MusicVolume", isOn ? 0f : -80f);
         PlayerPrefs.SetInt(SavedData.MusicToggle, isOn ? 1 : 0);
         PlayerPrefs.Save();
     }
     public void ToggleSFX(bool isOn) {
-        float defaultVolume = 1f; 
-        float volume = isOn ? defaultVolume : 0f; 
-        SetSFXVolume(volume);
-        PlayerPrefs.SetFloat(SavedData.SFXVolume, volume); 
+        audioMixer.SetFloat("SFXVolume", isOn ? 0f : -80f);
         PlayerPrefs.SetInt(SavedData.SFXToggle, isOn ? 1 : 0);
         PlayerPrefs.Save();
-    }
-
-    internal void SetUpToggle(object masterToggle, string v, Action<bool> value)
-    {
-        throw new NotImplementedException();
-    }
-        private void OnEnable() {
-        SceneManager.sceneLoaded += OnSceneLoaded;
-    }
-
-    private void OnDisable() {
-        SceneManager.sceneLoaded -= OnSceneLoaded;
-    }
-
-    private void OnSceneLoaded(Scene scene, LoadSceneMode mode) {
-        LoadSettings();
     }
 }
