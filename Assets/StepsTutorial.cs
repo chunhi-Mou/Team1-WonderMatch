7aaaada60c1551b669a449b607f815a3532935a0
Chu Nhi <chunhi.dev@gmail.com>
2025-05-10 19:54:37 +0700

    sorry

--- Assets/StepsTutorial.cs
+++ Assets/StepsTutorial.cs
@@ -1,10 +1,12 @@
 using UnityEngine;
+
+[System.Serializable]
 public class TutorialObj {
     public Card[] card;
     public GameObject[] powers;
-    //Undo -Shuffle - Magic - Cell Boost
+    // Undo - Shuffle - Magic - Cell Boost
 }
+
 public class StepsTutorial : MonoBehaviour {
-    [System.Serializable]
-    public static TutorialObj tutorialObj;
+    public TutorialObj tutorialObj;
 }
