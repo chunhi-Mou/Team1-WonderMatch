889efce41eba3cd75669e1c2c372b542945b9844
Chu Nhi <chunhi.dev@gmail.com>
2025-05-17 15:58:27 +0700

    100%

--- Assets/Scripts/CardSystem/CardOverlapChecker.cs
+++ Assets/Scripts/CardSystem/CardOverlapChecker.cs
@@ -1,30 +1,42 @@
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Card))]
 public class CardOverlapChecker : MonoBehaviour {
     private Card card;
     private List<Card> cardsBelow = new List<Card>();
-    private List<Card> cardsAbove = new List<Card>(); 
+    private List<Card> cardsAbove = new List<Card>();
 
     private BoxCollider boxCollider;
+
     private void Awake() {
         card = GetComponent<Card>();
         boxCollider = GetComponent<BoxCollider>();
     }
-    public void UpdateBelowTiles(float _deep = 1f) {
+
+    public void UpdateBelowTiles(float _deep = 5f) {
         cardsBelow.Clear();
 
         float shrinkFactor = 0.01f;
         SpriteRenderer sr = GetComponent<SpriteRenderer>();
-        Vector3 spriteSize = sr.bounds.size; 
-        Vector3 scaledSize = new Vector3(spriteSize.x * transform.localScale.x, spriteSize.y * transform.localScale.y, _deep);
-        Vector3 belowBoxSize = scaledSize - new Vector3(shrinkFactor, shrinkFactor, 0);
-        Vector3 belowBoxPosition = transform.position + new Vector3(0, 0, _deep);
 
-        Collider[] cards = Physics.OverlapBox(belowBoxPosition, belowBoxSize / 2, Quaternion.identity);
+        if (sr == null || sr.sprite == null) {
+            return;
+        }
 
-        foreach (var col in cards) {
+        Vector3 spriteWorldSize = sr.bounds.size;
+
+        Vector3 boxFullSize = new Vector3(
+            spriteWorldSize.x - shrinkFactor,
+            spriteWorldSize.y - shrinkFactor,
+            _deep                             
+        );
+
+        Vector3 boxCenterPosition = transform.position + new Vector3(0, 0, _deep / 2f);
+
+        Collider[] colliders = Physics.OverlapBox(boxCenterPosition, boxFullSize / 2f, Quaternion.identity);
+
+        foreach (var col in colliders) {
             if (col.gameObject != gameObject) {
                 Card otherCard = col.GetComponent<Card>();
                 if (otherCard != null) {
@@ -34,19 +46,28 @@ public class CardOverlapChecker : MonoBehaviour {
         }
     }
 
-    public void UpdateAboveTiles(float _deep = 1f) {
+    public void UpdateAboveTiles(float _deep = 5f) {
         cardsAbove.Clear();
 
         float shrinkFactor = 0.01f;
         SpriteRenderer sr = GetComponent<SpriteRenderer>();
-        Vector3 spriteSize = sr.bounds.size; 
-        Vector3 scaledSize = new Vector3(spriteSize.x * transform.localScale.x, spriteSize.y * transform.localScale.y, _deep);
-        Vector3 aboveBoxSize = scaledSize - new Vector3(shrinkFactor, shrinkFactor, 0);
-        Vector3 aboveBoxPosition = transform.position + new Vector3(0, 0, -_deep);
 
-        Collider[] cards = Physics.OverlapBox(aboveBoxPosition, aboveBoxSize / 2, Quaternion.identity);
+        if (sr == null || sr.sprite == null) {
+            return;
+        }
 
-        foreach (var col in cards) {
+        Vector3 spriteWorldSize = sr.bounds.size;
+
+        Vector3 boxFullSize = new Vector3(
+            spriteWorldSize.x - shrinkFactor,
+            spriteWorldSize.y - shrinkFactor,
+            _deep
+        );
+        Vector3 boxCenterPosition = transform.position + new Vector3(0, 0, -_deep / 2f);
+
+        Collider[] colliders = Physics.OverlapBox(boxCenterPosition, boxFullSize / 2f, Quaternion.identity);
+
+        foreach (var col in colliders) {
             if (col.gameObject != gameObject) {
                 Card otherCard = col.GetComponent<Card>();
                 if (otherCard != null) {
@@ -56,12 +77,12 @@ public class CardOverlapChecker : MonoBehaviour {
         }
     }
 
-
     public void NotifyTilesBelow() {
         foreach (var t in cardsBelow) {
-            t.GetComponent<CardOverlapChecker>().CheckIfUncovered();
+            t.GetComponent<CardOverlapChecker>()?.CheckIfUncovered();
         }
     }
+
     public void CheckIfUncovered() {
         UpdateAboveTiles();
         if (cardsAbove.Count == 0) {
@@ -69,5 +90,24 @@ public class CardOverlapChecker : MonoBehaviour {
         } else {
             card.SetSelectableData(false);
         }
+    }
+
+    private void OnDrawGizmosSelected() {
+        SpriteRenderer sr = GetComponent<SpriteRenderer>();
+        if (sr == null || sr.sprite == null) return;
+
+        float drawDeep = 5f;
+        float shrinkFactor = 0.01f;
+        Vector3 spriteWorldSize = sr.bounds.size;
+
+        Vector3 belowBoxFullSize = new Vector3(spriteWorldSize.x - shrinkFactor, spriteWorldSize.y - shrinkFactor, drawDeep);
+        Vector3 belowBoxCenterPosition = transform.position + new Vector3(0, 0, drawDeep / 2f);
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawWireCube(belowBoxCenterPosition, belowBoxFullSize);
+
+        Vector3 aboveBoxFullSize = new Vector3(spriteWorldSize.x - shrinkFactor, spriteWorldSize.y - shrinkFactor, drawDeep);
+        Vector3 aboveBoxCenterPosition = transform.position + new Vector3(0, 0, -drawDeep / 2f);
+        Gizmos.color = Color.magenta;
+        Gizmos.DrawWireCube(aboveBoxCenterPosition, aboveBoxFullSize);
     }
 }
